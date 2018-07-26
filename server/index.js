673129b926d5b3c7aec03e01f0b87d698f9d9146
Amy San Felipe <amysanfelipe@gmail.com>
2018-07-26 19:08:27 -0400

    add server paths to fetch and save links

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const parser = require('body-parser');
 const port = 3000;
+const db = '../database/index.js';
 
 
 app.use(parser.json());
@@ -11,4 +12,26 @@ app.use(express.static(__dirname + '/../client/dist'));
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`)
+})
+
+app.get('/navigation', (req, res) => {
+  db.fetchLinks()
+  .then((data) => {
+    res.status(200).end(data);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).end();
+  })
+})
+
+app.post('/navigation', (req, res) => {
+  db.saveLink(req.body.link)
+  .then(() => {
+    res.status(201).end();
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).end();
+  })
 })
